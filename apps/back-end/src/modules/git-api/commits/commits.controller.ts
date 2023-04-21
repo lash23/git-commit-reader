35f64225d7b8b09810beb61940684f21323bb864
Adrian Suarez <lsuarezh23@gmail.com>
2023-04-21 13:40:27 -0400

    :sparkles: feat: adding commits controller and GET endpoint

--- apps/back-end/src/modules/git-api/commits/commits.controller.ts
+++ apps/back-end/src/modules/git-api/commits/commits.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { BadGatewayException, Controller, Get, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { CommitsService } from './commits.service';
+import { Response } from 'express';
 
 @Controller('commits')
-export class CommitsController {}
+export class CommitsController {
+  constructor(
+    private commitsService: CommitsService
+  ) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getCommits(@Res() res: Response) {
+    try {
+      const data = await this.commitsService.getCommits();
+
+      return res.json({data});
+    } catch (error) {
+      return res.status(error.response.status).json(error.response.data)
+    }
+  }
+}
