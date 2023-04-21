cd847bff5bb2ad311e4daad275c522aba4f41d23
Adrian Suarez <lsuarezh23@gmail.com>
2023-04-21 12:54:05 -0400

    :heavy_plus_sign: feat: adding axios dependency and http module

--- apps/back-end/src/modules/git-api/commits/commits.module.ts
+++ apps/back-end/src/modules/git-api/commits/commits.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from "@nestjs/axios";
 import { CommitsController } from './commits.controller';
 
 @Module({
-  controllers: [CommitsController]
+  controllers: [CommitsController],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    })
+  ]
 })
 export class CommitsModule {}
