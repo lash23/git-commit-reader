042a219a4525e54b83ccad86f5556da66cfd493e
Adrian Suarez <lsuarezh23@gmail.com>
2023-04-21 12:30:28 -0400

    :sparkles: feat: adding git-api module & commits module to back-end app

--- apps/back-end/src/app/app.module.ts
+++ apps/back-end/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { GitApiModule } from "../modules/git-api/git-api.module";
 
 @Module({
-  imports: [],
+  imports: [GitApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ apps/back-end/src/modules/git-api/git-api.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CommitsModule } from './commits/commits.module';
+
+@Module({
+  imports: [CommitsModule]
+})
+export class GitApiModule {}
