36811d1e1dcf7b5594615382f7e18e197fc24eb0
Adrian Suarez <lsuarezh23@gmail.com>
2023-04-22 12:03:57 -0400

    :adhesive_bandage: chore: adding environment config to app module providers

--- apps/front-end/src/app/app.module.ts
+++ apps/front-end/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { AppRoutingModule } from './app-routing.module';
+import { APP_CONFIG } from '@git-commit-reader/app-config';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [AppRoutingModule, BrowserModule],
-  providers: [],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule],
+  providers: [{ provide: APP_CONFIG, useValue: environment }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
