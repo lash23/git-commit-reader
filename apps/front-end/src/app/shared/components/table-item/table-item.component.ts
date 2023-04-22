b774c4f095fda2b2ac872bef3c1cca543f3b1022
Adrian Suarez <lsuarezh23@gmail.com>
2023-04-22 13:25:47 -0400

    :sparkles: feat: adding table component & table item component

---
+++ apps/front-end/src/app/shared/components/table-item/table-item.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IListCommitItem } from '../../../../../../../interfaces/IListCommitItem';
+
+@Component({
+  selector: 'git-commit-reader-table-item',
+  templateUrl: './table-item.component.html',
+  styleUrls: ['./table-item.component.scss']
+})
+export class TableItemComponent implements OnInit {
+
+  @Input() commit: IListCommitItem | undefined;
+  avatar: string | undefined;
+  committerPage: string | undefined;
+  author: string | undefined;
+  message: string | undefined;
+  date: string | undefined;
+  sha: string | undefined;
+  commitUrl: string | undefined;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.commit);
+    this.setCommitDetails();
+  }
+
+  setCommitDetails() {
+    this.avatar = this.commit?.committer.avatar_url;
+    this.committerPage = this.commit?.committer.html_url;
+    this.author = this.commit?.committer.login;
+    this.message = this.commit?.commit.message;
+    this.date = this.commit?.commit.committer.date;
+    this.sha = this.commit?.sha;
+    this.commitUrl = this.parseCommitUrl(this.commit?.html_url!, this.sha!)
+  }
+
+  parseCommitUrl(htmlmUrl: string, sha: string): string {
+    const breakPoint = htmlmUrl.indexOf('commit/');
+    const subString = htmlmUrl.slice(0, breakPoint);
+    const url = `${subString}commit/${sha}`;
+
+    return url;
+  }
+}
--- apps/front-end/src/app/shared/shared.module.ts
+++ apps/front-end/src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableItemComponent } from './components/table-item/table-item.component';
+import { TableComponent } from './components/table/table.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableItemComponent,
+    TableComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TableItemComponent,
+    TableComponent,
   ]
 })
 export class SharedModule { }
